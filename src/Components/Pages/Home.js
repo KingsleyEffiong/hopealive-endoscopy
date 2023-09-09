653542a544a650ca2dae7110f2009e0d866251c7
Kingsley Effiong <kingsleyeffiong642@gmail.com>
2023-09-09 21:06:11 +0100

    added some css property

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -17,7 +17,7 @@ function Home() {
         </header>
         <main>
             <div className='main-container'>
-                <h1>Caring for the growing needs of our community</h1>
+                <h1 className='main-heading'>Caring for the growing needs of our community</h1>
                 <p className='main-text'>For maore than a decade, people all walks of life have found answers at Hopealive Endoscopy & Fertility Clinic located at the heart of the city, in addition a number of programs function outside the actual one. Here are some reasons why more than million people each year choose Hopealive Endoscopy & Fertility Clinc for medical container
                     
                     <ul>
@@ -28,9 +28,36 @@ function Home() {
                      </p>
             </div>
             <div className='main-container'>
-
+        <h1 className='main-heading'>Working Hours</h1>
+        <p className='box2'>
+            Caring for patients, every day of the week
+        </p>
+        <table >
+            <tr>
+                <td>Monday to Friday</td>
+                <td>7.00 to 24.00</td>
+            </tr>
+            <tr>
+                <td>Saturday</td>
+                <td>9.00 to 24.00</td>
+            </tr>
+            <tr>
+                <td>Sunday</td>
+                <td>9.00 to 24.00</td>
+            </tr>
+        </table>
             </div>
         </main>
+
+        <main>
+            <div className='main-container-2'>
+             <h1>About Us</h1>
+            </div>
+            <div className='main-container-2'>
+                <img src='/Images/building.jpg'></img>
+            </div>
+        </main>
+ 
 
     </>
   )
